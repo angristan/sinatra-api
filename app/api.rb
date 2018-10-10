5169b7d306a83cf1896515a3b28627142b1b1af7
angristan <angristan@pm.me>
2018-10-10 21:38:59 +0200

    Refactoring

--- app/api.rb
+++ app/api.rb
@@ -1,21 +1,19 @@
 require 'bundler/setup'
 require 'sinatra/base'
-require 'json'
 
 class Api < Sinatra::Base
+  file = File.read('data.json')
+  movies = JSON.parse(file)
+
   get '/hello' do
     'Hello world!'
   end
 
   get '/movies' do
-    file = File.read('data.json')
-    movies = JSON.parse(file)
     movies['movies'].to_s
   end
 
   get '/movies/:id' do
-    file = File.read('data.json')
-    movies = JSON.parse(file)
     movies['movies'].each do |movie|
       return movie.to_s if movie['id'] == params[:id].to_i
     end
